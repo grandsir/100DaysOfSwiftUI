09e01bfd5d2a538f9b57ced78bfe92bcb978da0d
GrandSir <69051988+GrandSir@users.noreply.github.com>
2021-07-31 12:42:43 +0300

    Update ContentView.swift

--- Project-2-Guess The Flag/ContentView.swift
+++ Project-2-Guess The Flag/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct FlagImage: View {
     var image: String
+    @Binding var color: Color
     var body: some View {
         Image(image)
             .renderingMode(.original)
@@ -17,19 +18,42 @@ struct FlagImage: View {
                         .foregroundColor(Color.black)
                         .opacity(0.35))
             .overlay(Capsule()
-                        .stroke(Color.red, lineWidth: 4))
-            .shadow(color: .red, radius: 10)
+                        .stroke(color, lineWidth: 4))
+            .shadow(color: color, radius: 10)
     }
 }
 
 struct ContentView: View {
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
+    
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     
     @State private var correctAnswer = Int.random(in: 0...2)
     @State private var showingScore = false
     @State private var scoreTitle = ""
     @State private var tappedFlag = 0
-    @State private var score = 0
+    @State private var score = 0 {
+         didSet {
+            if score < 0 {
+                score = 0
+            }
+        }
+    }
+    @State private var animationAmount = 0.0
+    @State private var fadeAmount = 1.0
+    @State private var color = Color.red
     
     var body: some View {
         ZStack{
@@ -41,44 +65,65 @@ struct ContentView: View {
                         .scaleEffect(1.4)
                     Text(countries[correctAnswer])
                         
-                        .foregroundColor(.red)
+                        .foregroundColor(color)
                         .font(.largeTitle)
                         .fontWeight(.black)
                         .padding()
                         .scaleEffect(1.2)
-                        .shadow(color: .red, radius: 5)
+                        .shadow(color: color, radius: 5)
 
                 }
                 Spacer()
                 ForEach(0 ..< 3) {number in
                     Button(action: {
+                        self.fadeAmount = 1.0
                         self.flagTapped(number)
                         self.tappedFlag = number
-                        print(correctAnswer)
-                        print(number)
-                        print(self.countries[number])
-                        print(self.countries)
+                        withAnimation(Animation.easeInOut(duration: 0.5)) {
+                            self.animationAmount += 360
+                        }
+                        self.fadeAmount = 0.25
+                        
+                        if tappedFlag != correctAnswer {
+                            self.color = .gray
+                        }
+
                     }) {
-                        FlagImage(image: self.countries[number]) // Day 24 Challange
+                        FlagImage(image: self.countries[number], color: $color)
+                            .opacity(tappedFlag == number ? 1 : fadeAmount)
+                            .rotation3DEffect(
+                                .degrees(correctAnswer == tappedFlag && correctAnswer == number ? animationAmount : 0.0),
+                                axis: (x: 0.0, y: 1.0, z: 0.0)
+                            )
                     }
+                    .accessibility(label: Text( self.labels[self.countries[correctAnswer], default: "Unknown Flag"]))
                 }
                 Spacer()
                 Spacer()
+                Text("Your Score is: \(score)")
+                    .font(.headline)
+                    .font(.system(size: 30))
+                    .foregroundColor(color)
+                    .shadow(color: color, radius:  5)
+                    .padding()
             }
         }.alert(isPresented: $showingScore) {
-            Alert(title: Text(scoreTitle), message: scoreTitle == "Wrong!" ? Text("Wrong! that's the flag of \(countries[tappedFlag])"): Text("Your score is \(score)"), dismissButton: .default(Text("Continue")) {
+            Alert(title: Text(scoreTitle), message: Text(scoreTitle == "Wrong!" ? "Oops, this is the flag of \(self.countries[tappedFlag])": "Your score is now \(score)"), dismissButton: .default(Text("Continue")) {
                 self.askQuestion()
+                self.fadeAmount = 1
+                self.color = .red
             }
             )}
     }
     
     func flagTapped(_ number: Int) {
         if number == correctAnswer{
-            scoreTitle = "Correct"
-            score += 1
+            self.scoreTitle = "Well done!"
+            self.score += 1
         }
         else {
-            scoreTitle = "Wrong!"
+            self.scoreTitle = "Wrong!"
+            self.score -= 3
         }
         showingScore = true
     }
