1274236e83d38da3ac6b819ab2e397cd7bccef76
GrandSir <69051988+GrandSir@users.noreply.github.com>
2021-08-02 09:39:11 +0300

    Add files via upload

---
+++ Project-8-iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by GrandSir on 11.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
